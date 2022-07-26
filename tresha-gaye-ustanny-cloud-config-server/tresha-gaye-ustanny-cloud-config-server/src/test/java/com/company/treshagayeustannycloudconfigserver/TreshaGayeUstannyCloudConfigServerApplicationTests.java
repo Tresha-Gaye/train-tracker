eab837c3436fc7dcf6b4c5b29d62265e5b279a5f
Twigster <treshagaye@gmail.com>
2022-07-26 12:24:53 -0400

    first commit

---
+++ tresha-gaye-ustanny-cloud-config-server/tresha-gaye-ustanny-cloud-config-server/src/test/java/com/company/treshagayeustannycloudconfigserver/TreshaGayeUstannyCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.treshagayeustannycloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreshaGayeUstannyCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
